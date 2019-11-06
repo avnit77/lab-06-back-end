1fcaf905c554c14829a6dc3b7c30b6fe608ccd42
Tali Avni <tali@aristo7.com>
2019-11-06 12:53:59 -0800

    added APIs

--- server.js
+++ server.js
@@ -1,71 +1,76 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(express.static('./public'));
 
-app.get('/location', (request, response) => {
-    try {
-        const location = request.query.location;
-        const result = getLatLng(location);
-        response.status(200).json(result);
-    }
-    catch (err) {
-        response.status(500).send('Sorry something went wrong, please try again');
-    }
-});
 
-const geoData = require('./data/geo.json');
+let latlngs;
 
-function getLatLng(location) {
-    if (location === 'bad location') {
-        throw new Error();
-    }
-
-    return toLocation();
-}
-
-function toLocation() {
-    const firstResult = geoData.results[0];
-    const geometry = firstResult.geometry;
+const formatLocationResponse = locationItem => {
+    const {
+        geometry: {
+            location: {
+                lat,
+                lng,
+            },
+        },
+        formatted_address,
+    } = locationItem;
 
     return {
-        formatted_query: firstResult.formatted_address,
-        latitude: geometry.location.lat,
-        longitude: geometry.location.lng
+        formatted_query: formatted_address,
+        latitude: lat,
+        longitude: lng,
     };
-}
+};
 
-const darkSky = require('./data/darksky.json');
+const getWeatherResponse = async(lat, long) => {
+    const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
 
-app.get('/weather', (request, response) => {
+    const weatherItem = await superagent.get(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lat},${long}`);
+
+    const actualWeatherData = JSON.parse(weatherItem.text);
+    const dailyArray = actualWeatherData.daily.data.map((item) => {
+        return {
+            forecast: item.summary,
+            time: new Date(item.time * 1000).toDateString(),
+        };
+    });
+    return dailyArray;
+};
+
+app.get('/location', async(req, res) => {
     try {
-        const location = toLocation();
-        const weather = getWeather(location);
-        response.status(200).json(weather);
-    }
-    catch (err) {
-        response.status(500).send('error, we could not get weather details');
+        const searchQuery = req.query.search;
+        const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+
+        const locationItem = await superagent.get (`https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${GEOCODE_API_KEY}`);
+        const actualItem = JSON.parse(locationItem.text).results[0];
+        const response = formatLocationResponse(actualItem);
+
+        latlngs = response;
+
+        res.json(response);
+    } catch (e) {
+        throw new Error(e);
     }
 });
 
-function getWeather() {
-    const forecast = [];
+app.get('/weather', async(req, res) => {
+    try {
+        const weatherObject = await getWeatherResponse(latlngs.latitude, latlngs.longitude);
 
-    darkSky.daily.data.forEach(day => {
-        let date = new Date(day.time);
-        let dateString = date.toDateString();
-        forecast.push({
-            'forecast': day.summary,
-            'time': dateString
-        });
-    });
-    return forecast;
-}
+        res.json(weatherObject);
+    } catch (e) {
+        throw new Error(e);
+    }
+});
 
 app.listen(PORT, () => {
 });
